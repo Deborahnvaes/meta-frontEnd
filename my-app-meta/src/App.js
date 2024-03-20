f9577a67d2e30ab05a68d6ed0a9e5a789966209a
Déborah Novaes <deborahsegutec@hotmail.com>
2024-03-20 20:16:24 -0300

    adicionando component ao app.js

--- my-app-meta/src/App.js
+++ my-app-meta/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { Header } from './Componentes/header/Header';
-
+import { CallToAction } from './Componentes/callToAction/CallToAction.jsx';
 
 function App() {
   return (
-    <>
+  <>
     <Header/>
+    <CallToAction/>
     </>
   );
 }
