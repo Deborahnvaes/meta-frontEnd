990206c0bc17d46a854c4b136c856c28e8639086
Déborah Novaes <deborahsegutec@hotmail.com>
2024-04-01 18:17:56 -0300

    Ajuste grid

--- my-app-meta/src/Componentes/header/Header.jsx
+++ my-app-meta/src/Componentes/header/Header.jsx
@@ -1,49 +1,55 @@
-import React from 'react'
-import minhaLogo from '../../assets/imagens/Logo.svg';
-import { Container, Row, Col } from 'react-bootstrap';
-import styled from 'styled-components'; 
+import React from "react";
+import minhaLogo from "../../assets/imagens/Logo.svg";
+import { Container, Row, Col, Navbar } from "react-bootstrap";
+import styled from "styled-components";
 
 const HeaderContainer = styled.header`
-background-color: #fff;
-padding: 20px 0;
+  padding: 20px 0;
 `;
 
-const StyledNav = styled.nav `
-color: #333333;
+const StyledNav = styled.nav`
+  display: flex;
+  justify-content: flex-end;
 
-a {
-  color: black;
-  text-decoration: none;
-  transition: color 0.3s ease;
-  margin-right: 20px;
-
-  &:hover{
-    color: #ddd;
+  a {
+    color: #333;
+    text-decoration: none;
+    margin-left: 15px;
+    font-size: 16px;
   }
-}
 
+  a:hover {
+    color: #ff6600;
+  }
 `;
 
 export const Header = () => {
   return (
-  <HeaderContainer>
-    <Container>
-      <Row className="align-items-center">
-        <Col xs={8}> 
-        <img className='logo' src={minhaLogo} alt="logo-empresa" />
-        </Col>
-        <Col xs={4}>
-        <StyledNav>
-          <a href="#">INÍCIO</a>
-          <a href="#">SOBRE</a>
-          <a href="#">MENU</a>
-          <a href="#">RESERVAS</a>
-          <a href="#">PEDIDOS</a>
-          <a href="#">ENTRAR</a>
-        </StyledNav>
-        </Col>
+    <HeaderContainer>
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={8}>
+            <img className="logo" src={minhaLogo} alt="logo-empresa" />
+          </Col>
+          <Col xs={4}>
+            <StyledNav>
+              <Navbar expand="lg" variant="dark">
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                  <StyledNav className="ml-auto">
+                    <a href="#">INÍCIO</a>
+                    <a href="#">SOBRE</a>
+                    <a href="#">MENU</a>
+                    <a href="#">RESERVAS</a>
+                    <a href="#">PEDIDOS</a>
+                    <a href="#">ENTRAR</a>
+                  </StyledNav>
+                </Navbar.Collapse>
+              </Navbar>
+            </StyledNav>
+          </Col>
         </Row>
-        </Container>
-  </HeaderContainer>
-  )
-}
+      </Container>
+    </HeaderContainer>
+  );
+};
