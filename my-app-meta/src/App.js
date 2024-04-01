a7e556a8c543f336cccf73baadfb89dbabf1b93e
Déborah Novaes <deborahsegutec@hotmail.com>
2024-04-01 18:17:14 -0300

    Ajuste da chamada do componente

--- my-app-meta/src/App.js
+++ my-app-meta/src/App.js
@@ -1,9 +1,9 @@
 
 import './App.css';
 import { Header } from './Componentes/header/Header';
-import { CallToAction } from './Componentes/callToAction/CallToAction.jsx';
+// import { CallToAction } from './Componentes/callToAction/CallToAction.jsx';
 import { Specials } from './Componentes/specials/Specials.jsx';
-import HomePage from './Componentes/homePage/HomePage.jsx';
+import HomePage from './Componentes/homePage/HomePage.jsx'
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
     <Header/>
     <HomePage />
     <Specials/>
+    
     </>
   );
 }
